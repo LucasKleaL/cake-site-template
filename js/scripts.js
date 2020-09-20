a3f81dedac952b160c079e5009e94baad70c1e37
LucasKleaL <lucaskleal222@outlook.com>
2020-09-20 00:56:34 -0300

    2 commit
    Melhoria no template da página principal (home, index.html).

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
+        arrows: true,
         prevArrow: $("#seta-prev"),
         nextArrow: $("#seta-next"),
-        arrows: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        
     });
 
     $(".carousel").on("afterChange", function(){
@@ -22,11 +22,11 @@ $(document).ready(function(){
     $(".carousel").on('init', function(){
         $("#nomeProduto").text($(".slick-center").data("name"));
         $("#precoProduto").text($(".slick-center").data("price"));
-      });
+    });
 
-      $(".carousel").on('reInit', function(){
+    $(".carousel").on('reInit', function(){
         $("#nomeProduto").text($(".slick-center").data("name"));
         $("#precoProduto").text($(".slick-center").data("price"));
-      });
+    });
 
 });
